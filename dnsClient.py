085b190157e879c9ba49a9679125c9e4d8fc47df
Elizaveta Abrosimova <144963902+AbrosimovaE@users.noreply.github.com>
2023-11-13 16:57:53 -0500

    dnsClient.py

--- dnsClient.py
+++ dnsClient.py
@@ -81,7 +81,7 @@ def dns_query(type, name, server):
         # 3. edu (length of 3)
         
     qname_parts = name.split('.') # How can we easily split the string?
-    qname_encoded_parts = [struct.pack('B', len(part)) + part.encode('ascii') for part in qname_parts] # Make sure it's encoded as a sequence of the right character encoding type (lowercase)
+    qname_encoded_parts = [struct.pack('B', len(part)) + part.encode('utf-8') for part in qname_parts] # Make sure it's encoded as a sequence of the right character encoding type (lowercase)
     qname_encoded = b''.join(qname_encoded_parts) + b'\x00' #enter the closing byte value to signify the end of the domain string (two digits)
 
     # Encode the QTYPE and QCLASS
@@ -150,12 +150,12 @@ def dns_query(type, name, server):
         rdata = response_answer[offset:offset+rdlength]
         offset += rdlength
 
-        if type == 'A': # Lookup Type value
+        if type == 1: # Lookup Type value
             # A record (IPv4 address)
             ipv4 = socket.inet_ntop(socket.AF_INET, rdata)
             print(f'{name} has IPv4 address {ipv4}')\
             return ipv4
-        elif type == 'AAAA': # Lookup Type value
+        elif type == 28: # Lookup Type value
             # AAAA record (IPv6 address)
             ipv6 = socket.inet_ntop(socket.AF_INET6, rdata)
             print(f'{name} has IPv6 address {ipv6}')
